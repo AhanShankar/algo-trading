5f4aad969c43add109ac9e659291cff7ae7bcf28
AhanShankar <ahan.srivastava@smallcase.com>
2026-06-28 16:57:03 +0530

    fix: remove deprecated seed call to RNG

--- src/utils/utils.go
+++ src/utils/utils.go
@@ -2,13 +2,10 @@ package utils
 
 import (
 	"math/rand"
-	"time"
 )
 
 func RandomCapsString() string {
 	letters := []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-
-	rand.Seed(time.Now().UnixNano())
 
 	result := make([]rune, 5)
 	for i := 0; i < 5; i++ {
